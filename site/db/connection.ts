1414ea2f8040b2372636e2c68a04bed28ec554d4
mbeka02 <antonymbeka@gmail.com>
2026-09-23 13:36:33 +0300

    updated db connection code

--- site/db/connection.ts
+++ site/db/connection.ts
@@ -15,16 +15,13 @@ const options: MongoClientOptions = {
 
 let client: MongoClient;
 // Use a global variable in dev due to HMR
-if (process.env.NODE_ENV === "development") {
-  let globalWithMongo = global as typeof globalThis & {
-    _mongoClient?: MongoClient;
-  };
-  if (!globalWithMongo._mongoClient) {
-    globalWithMongo._mongoClient = new MongoClient(mongodb_uri, options);
-  }
-  client = globalWithMongo._mongoClient;
-} else {
-  client = new MongoClient(mongodb_uri, options);
+let globalWithMongo = global as typeof globalThis & {
+  _mongoClient?: MongoClient;
+};
+if (!globalWithMongo._mongoClient) {
+  globalWithMongo._mongoClient = new MongoClient(mongodb_uri, options);
+}
+client = globalWithMongo._mongoClient;
 }
 export async function testConnection() {
   try {
